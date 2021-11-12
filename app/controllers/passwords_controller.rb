ada5cb8e95750b5e3a14093aaa6a6c0bd7061567
Jean TOURE <jean.toure@ngser.com>
2021-11-12 10:18:20 +0000

    Feat: Password reset

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,17 @@
 class PasswordsController < Devise::PasswordsController
-  layout "devise"
+  layout 'devise'
+
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    flash[:alert] = 'Adresse email inconnue!' unless resource.persisted?
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
 
   def update
     super
