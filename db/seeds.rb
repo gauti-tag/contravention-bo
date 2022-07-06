038696ae660ca951ee396598aa34fc404410538d
Gautier Tiehoule <gautier.tiehoule@ngser.com>
2022-07-06 11:36:02 +0000

    Refactor: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 admin_profile = AdminProfile.create(title: 'Administrateur')
 guest_profile = AdminProfile.create(title: 'Guest')
-User.create(email: 'jean.toure@ngser.com', password: 'lotogb21', firstname: 'Jean', lastname: 'TOURE', msisdn: '2250749586809', profile_id: admin_profile.id, account_status: 1)
-User.create(email: 'gautiertiehoule_stagiairedti@ngser.com', password: 'contragb21',  lastnaname: 'TIEHOULE', msisdn: '2250708345891', profile_id: admin_profile.id, account_status: 1)
+User.create(email: 'gautier.tiehoule@ngser.com', password: 'contragb22', firstname: 'Gautier', lastname: 'Tiehoule', msisdn: '2250749586809', profile_id: admin_profile.id, account_status: 1)
+User.create(email: 'gautier.tiehoule@ngser.com', password: 'contragb21',  lastname: 'TIEHOULE', msisdn: '2250708345891', profile_id: admin_profile.id, account_status: 1)
 User.create(email: 'projet@ngser.com', password: 'lotogb21', firstname: '', lastname: 'Projet', msisdn: '2250757462025', profile_id: guest_profile.id, account_status: 1)
