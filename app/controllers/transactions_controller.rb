9cf7ef11359b2e87a4521e35b169bc5785ef180e
TRAORE YACOUBA <traoreyacouba6946@gmail.com>
2022-11-16 14:07:08 +0000

    Feat: Display transaction details

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,10 +6,40 @@ class TransactionsController < ApplicationController
 
 
   def show 
-
+    model = params[:model]
+    result = DatatablesManager::FetchRecord.call(transaction: { model: model_name(model), id: params[:id]} )
+    #redirect_back(fallback_location: root_path) if result['status'] != 200
+    @record = result['data']
+    @back_url = back_url(model)
+    render transaction_view(model)
   end
 
   private
+
+  def transaction_view(name)
+    case name
+    when 'ticket_payments'
+        'show_payment'
+    when 'cnis'
+        ''
+    when 'civil_acts'
+        ''
+    else
+        ''
+    end
+  end
+
+  def model_name(name)
+    {
+    'ticket_payments' => 'TrafficTicketPayment',
+    'cnis' => 'cnis',
+    'civil_acts' => 'civil_acts'
+    }.fetch(name)
+  end
+
+  def back_url(name)
+    "/transactions/contravention/#{name}"
+  end
 
 
 end
