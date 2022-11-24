4a4c1f6ef01276cb353a1284f62a3f9e3c78e87c
Gautier Tiehoule <gautier.tiehoule@ngser.com>
2022-11-24 10:57:13 +0000

    Feat: parameters views and model

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -1,2 +1,7 @@
 class Parameter < ApplicationRecord
+    extend FriendlyId
+    friendly_id :name, use: :history
+
+    validates :name, presence: true, uniqueness: true
+    validates :slug, presence: true, uniqueness: true
 end
